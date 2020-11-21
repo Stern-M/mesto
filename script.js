ff1454b36e618935d9d0a044d94bc0ef170e7287
Darina Bandurina <dari-mne@yandex.ru>
2020-11-21 14:54:12 +0300

    editform: style done & edit button workable

--- script.js
+++ script.js
@@ -1,7 +1,37 @@
-function openForm() {
-  document.getElementById("myForm").style.display = "block";
+const ProfileEditButton = document.querySelector('.profile__edit_button');
+const PopupNode = document.querySelector('.popup');
+const PopupCancelButtonNode = document.querySelector('.popup__cancel_button');
+
+const ProfileTitleNode = document.querySelector('.profile__title');
+const ProfileSubTitleNode = document.querySelector('.profile__subtitle');
+const SaveButtonNode = document.querySelector('.popup__save_button');
+
+
+
+ProfileEditButton.addEventListener('click', togglePopupVisibility);
+PopupCancelButtonNode.addEventListener('click', togglePopupVisibility);
+SaveButtonNode.addEventListener('click', togglePopupVisibility);
+
+function togglePopupVisibility() {
+  PopupNode.classList.toggle('popup_visible');
 }
 
-function closeForm() {
-  document.getElementById("myForm").style.display = "none";
+const form1 = document.querySelector('.popup__container');
+const form2 = document.querySelector('.popup__container');
+
+form1.addEventListener('submit', NameFormSubmit) & ('click', togglePopupVisibility);
+form2.addEventListener('submit', descriptionFormSubmit);
+
+function NameFormSubmit(event) {
+  console.log(event.currentTarget);
+  event.preventDefault();
+  const TitleInputNode = event.currentTarget.querySelector('.popup__name');
+  ProfileTitleNode.textContent = TitleInputNode.value;
+}
+
+function descriptionFormSubmit(event) {
+  console.log(event.currentTarget);
+  event.preventDefault();
+  const SubInputNode = event.currentTarget.querySelector('.popup__description');
+  ProfileSubTitleNode.textContent = SubInputNode.value;
 }
