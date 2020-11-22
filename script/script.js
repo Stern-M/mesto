9c2b50ecd767841a1bcff5cc092c02401c94d48c
Darina Bandurina <dari-mne@yandex.ru>
2020-11-23 00:55:01 +0300

    hotfixes done

--- script/script.js
+++ script/script.js
@@ -4,31 +4,32 @@ let popupCancelButtonNode = document.querySelector('.popup__cancel-button');
 let profileTitleNode = document.querySelector('.profile__title');
 let profileSubTitleNode = document.querySelector('.profile__subtitle');
 let saveButtonNode = document.querySelector('.popup__save-button');
-let forms = document.querySelector('.popup__data');
+let forms = document.querySelector('.popup__container');
 let titleInputNode = document.querySelector('.popup__data_name');
 let subInputNode = document.querySelector('.popup__data_description');
 
 function togglePopupVisibility() {
   popupNode.classList.toggle('popup_visible');
+  titleInputNode.value = profileTitleNode.textContent;
+  subInputNode.value = profileSubTitleNode.textContent;
 }
 
-profileEditButton.addEventListener('click', togglePopupVisibility);
-popupCancelButtonNode.addEventListener('click', togglePopupVisibility);
-/*saveButtonNode.addEventListener('submit', togglePopupVisibility);*/
-
-/*forms.forEach((formNode) => {
-    formNode.addEventListener('submit', popFormSubmit);
-});*/
-
-forms.addEventListener('submit', popFormSubmit);
+function togglePopupVisibilitynone() {
+  popupNode.classList.toggle('popup_visible');
+}
 
 function popFormSubmit(event) {
   event.preventDefault();
-  titleInputNode.value = profileTitleNode.textContent;
-  subInputNode.value = profileSubTitleNode.textContent;
-  profileTitleNode.textContent = event.currentTarget.titleInputNode.value;
-  profileSubTitleNode.textContent = event.currentTarget.subInputNode.value;
+  profileTitleNode.textContent = titleInputNode.value;
+  profileSubTitleNode.textContent = subInputNode.value;
+  popupNode.classList.toggle('popup_visible');
 }
+
+profileEditButton.addEventListener('click', togglePopupVisibility);
+popupCancelButtonNode.addEventListener('click', togglePopupVisibilitynone);
+forms.addEventListener('submit', popFormSubmit);
+
+
 
 
 
